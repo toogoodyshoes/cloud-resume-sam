2e78cd56b95823b8d0c5da30b00522837ce2e36d
toogoodyshoes <lekhadenihar11@gmail.com>
2024-02-26 00:57:06 +0530

    Init commit

---
+++ update_count/count.py
@@ -0,0 +1,46 @@
+import boto3
+
+dynamodb = boto3.resource('dynamodb')
+
+def increment(event, context):
+    table = dynamodb.Table('visitor-counter')
+    
+    response = table.get_item(
+        Key={'user': 'nihar'}
+    )
+    
+    if 'Item' in response:
+        response.clear()
+        response = table.update_item(
+            Key={'user': 'nihar'},
+            UpdateExpression='SET v_count = v_count + :val',
+            ExpressionAttributeValues={
+                ':val': 1
+            }
+        )
+    else:
+        response.clear()
+        response = table.put_item(
+            Item={
+                'user': 'nihar',
+                'v_count': 1,
+            }
+        )
+        
+    if response['ResponseMetadata']['HTTPStatusCode'] == 200:
+        res = table.get_item(
+            Key={'user': 'nihar'}
+        )
+        count = res['Item']['v_count']
+        http_res = {
+                "message": {
+                    "visitor_count": count,
+                }
+            }
+    else:
+        http_res = {
+                "message": "Update operation failed!"
+            }
+        
+
+    return http_res
